c178d28a431f70771ab81f3ffe30a16a09a09d9b
AdminingSS <admining@mail.ru>
2019-08-30 16:46:14 +0300

    optimisation 1

--- src/scripts/vendors/index.js
+++ src/scripts/vendors/index.js
@@ -1,10 +1,8 @@
 import '@babel/polyfill';
 
 import './uikit';
-import './fontawesome';
-import './select2';
+// import './fontawesome';
 import './slick';
 import './tabs';
 import './customslider';
 import './yandex';
-//import './jqvalidate';
--- src/scripts/vendors/slick.js
+++ src/scripts/vendors/slick.js
@@ -3,28 +3,17 @@ import 'slick-carousel/slick/slick.less';
 import 'slick-carousel/slick/slick-theme.less';
 
 $(() => {
-
     (() => {
-
         const $tabsWrapper = $('.tm-tabs-wrapper');
-
         const $sliderTop = $('.tm-slider-top-body');
         const $prevArrowTop = $('.tm-slider-top-wrap .tm-button-round-left');
         const $nextArrowTop = $('.tm-slider-top-wrap .tm-button-round-right');
-        const $sliderTopImages = $('.tm-slider-top-image div');
-        // $sliderTopImages.each(function () {
-        //     $(this).css({
-        //         left: '250%'
-        //     }).removeClass('uk-hidden');
-        // });
+        const $sliderTopImages = $('.tm-slider-top-image >div');
+        const $sliderTopImage = [];
         const $image = $($sliderTopImages[0]);
-
         const $sliderSales = $tabsWrapper.find('.tm-tab-slider');
         const $prevArrowSales = $('.tm-tabs-sales .tm-button-round-left');
         const $nextArrowSales = $('.tm-tabs-sales .tm-button-round-right');
-
-        //const $sliderAbout = $('.tm-about-slider');
-
         const sliderTopOptions = {
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -59,12 +48,12 @@ $(() => {
                 }
             ]
         };
-        // const sliderAboutOptions = {
-        //     slidesToShow: 1,
-        //     slidesToScroll: 1,
-        //     prevArrow: '.tm-about-slider-controls .tm-button-round-left',
-        //     nextArrow: '.tm-about-slider-controls .tm-button-round-right'
-        // };
+
+        $sliderTopImages.each(function () {
+            $sliderTopImage.push($(this));
+        });
+
+        $sliderTopImages.removeClass('uk-hidden');
 
         $sliderTop.on('init', loadFirstImage);
         $sliderTop.on('beforeChange', function (event,slick,currentSlide) {
@@ -74,81 +63,8 @@ $(() => {
             loadImage(currentSlide);
         });
 
-        function loadFirstImage() {
-
-
-            // $image.css({
-            //     left: '250%',
-            //     // opacity: 0,
-            //     // transition: 'left 2s, opacity 2s'
-            // });
-            // $image.removeClass('uk-hidden');
-
-            setTimeout(function () {
-                $image.addClass('tm-active');
-            },10);
-
-        }
-
-        function removeImage (num) {
-            $($sliderTopImages[num]).removeClass('tm-active');
-        }
-
-        function loadImage (num) {
-            $($sliderTopImages[num]).addClass('tm-active');
-        }
-
-        // $sliderTop.on('init', function () {
-        //     //const $image = $sliderTop.find('.slick-current img');
-        //
-        //     //$image.css('width',0);
-        //     // $image.css('left','150%');
-        //     // $image.animate({
-        //     //     //width: '100%',
-        //     //     left: '50%'
-        //     // }, 2000);
-        //
-        //     // $image.animate({  textIndent: 0 /* или любое другое не очень-то нужное здесь свойство */ }, {
-        //     //     step: function(now, fx) {
-        //     //         $(this).css('transform','translateX(' + now + '%)');
-        //     //     },
-        //     //     duration: 'slow'
-        //     // }, 'linear');
-        //
-        //     // console.log($image);
-        //     // $image.removeClass('slick-active').delay(1000).addClass('slick-active');
-        //     //
-        //     // //$image.addClass('no-transition');
-        //     //$image.addClass('animate-on-start');
-        //     // setTimeout(function () {
-        //     //     $image.addClass('animate-on-start');
-        //     // }, 800);
-        //
-        //
-        //     // $image.removeClass('no-transition');
-        //     // $image.addClass('reanimate-on-start');
-        //     // $image.animate({
-        //     //     opacity: 1
-        //     // }, 5000, function() {
-        //     //     // Animation complete.
-        //     // });
-        // });
-
-        // $sliderTop.on('beforeChange', function () {
-        //
-        //     const $image = $(this).find('.slick-current img');
-        //     console.log($image);
-        //     $image.animate({
-        //         left: '150%'
-        //     }, 800, function () {
-        //         $image.css('left','50%');
-        //     });
-        //
-        // });
-
         $sliderTop.slick(sliderTopOptions);
         $sliderSales.slick(sliderSalesOptions);
-        //$sliderAbout.slick(sliderAboutOptions);
 
         $prevArrowTop.on('click', sliderPause);
         $nextArrowTop.on('click', sliderPause);
@@ -157,11 +73,28 @@ $(() => {
             const $activeSlider = $tabsWrapper.find('.uk-active .tm-tab-slider');
             $activeSlider.slick('slickPrev');
         });
-
         $nextArrowSales.on('click', function () {
             const $activeSlider = $tabsWrapper.find('.uk-active .tm-tab-slider');
             $activeSlider.slick('slickNext');
         });
+
+        function loadFirstImage() {
+
+            setTimeout(function () {
+                $image.addClass('tm-active');
+            },500);
+
+        }
+
+        function removeImage (num) {
+            //$($sliderTopImages[num]).removeClass('tm-active');
+            $sliderTopImage[num].removeClass('tm-active');
+        }
+
+        function loadImage (num) {
+            //$($sliderTopImages[num]).addClass('tm-active');
+            $sliderTopImage[num].addClass('tm-active');
+        }
 
         function sliderPause() {
             $sliderTop.slick('slickPause');
@@ -171,5 +104,3 @@ $(() => {
         }
     })();
 });
-
-
